757a35cd1988f315eb6f7d64913f5e593cfbb0f5
ljsikuade <40580944+ljsikuade@users.noreply.github.com>
2018-10-11 14:05:34 +0100

    reached stretch goals

--- src/index.js
+++ src/index.js
@@ -37,7 +37,7 @@ function stringVariator() {
   let existingString = "";
   function inner(stringType) {
     if (typeof stringType === "string") {
-      existingString.concat(stringType);
+      existingString += stringType;
     } else if (Array.isArray(stringType)) {
       existingString.concat(stringType.join(" "));
     }
@@ -46,4 +46,158 @@ function stringVariator() {
   return inner;
 }
 
-module.exports = { increase, double, totalArray };
+function mergeSort() {
+  emptyArray = [4, 12, 8];
+
+  function insertNumber(number) {
+    emptyArray.push(number);
+    emptyArray.sort((a, b) => a - b);
+    return emptyArray.pop();
+  }
+  return insertNumber;
+}
+
+function calculator() {
+  total = 0;
+
+  function insertNumber(number, operator, otherNumber) {
+    let operatorObject = {
+      "+": function(x, y) {
+        return x + y;
+      },
+      "-": (x, y) => {
+        return x - y;
+      },
+      "*": function(x, y) {
+        return x * y;
+      }
+    };
+    switch (operator) {
+      case "+":
+        total += operatorObject["+"](number, otherNumber);
+        break;
+      case "-":
+        total += operatorObject["-"](number, otherNumber);
+        break;
+      case "*":
+        total += operatorObject["*"](number, otherNumber);
+    }
+    return total;
+  }
+  return insertNumber;
+}
+
+// dog = {
+// name: 'rex',
+// breed: 'Poodle',
+// colour: 'Blue',
+// location: 'Ipswich'
+// }
+
+function dogHome() {
+  let locations = {};
+
+  obj = {
+    houseDog: function(dog) {
+      if (locations[dog.location]) {
+        locations[dog.location].push(dog);
+      } else {
+        locations[dog.location] = [dog];
+      }
+      return locations;
+    },
+    getDogsByLocation: function(location) {
+      return locations.location;
+    }
+  };
+
+  return obj;
+}
+
+/*
+  person = {
+    name: Bob, 
+    money: 12
+  }
+
+*/
+
+function trainStation() {
+  let people = [];
+  const randomAmount = Math.floor(Math.random() * Math.floor(20));
+  const randomPerson = Math.floor(Math.random() * Math.floor(people.length));
+
+  trainControl = {
+    arrive: function(person) {
+      people.push(person);
+    },
+    getPeople: function() {
+      return people;
+    },
+    giveMoney: function(randomAmount, randomPerson) {
+      people[randomPerson].money += randomAmount;
+    },
+    trainArrives: function() {
+      const theOnePercent = people.filter(person => person.money > 20);
+      return theOnePercent;
+    }
+  };
+
+  return trainControl;
+}
+
+// [{name: 'Milk, Quanitity: 10, Price: 1.00 }, {name: 'Crisps, Quanitity: 10, Price: 0.75 }]
+
+function shop() {
+  let storage = {};
+  let totalRevenue = 0; 
+
+  let obj = {
+    addStock: function (arrayOfItems) {
+     arrayOfItems.map(function(item){
+       if(storage[item].name) {
+        storage[item].quantity += item.quantity 
+        if (storage[item].price !== item.price) {
+         const newPrice = (storage[item].price + item.price) / 2
+
+         storage[item].price = newPrice;
+        }
+       } else {
+         storage[item] = item
+       }
+     })
+
+   
+  },
+
+  sellStock: function (orderArray) {
+    orderRevenue = 0;
+   let receipt =  orderArray.map(function(order){
+     if (storage[order.name]) {
+       if (order.quantity > storage[order.name].quantity) {
+        let difference = order.quantity - storage[order.name].quantity
+        order.quantity = order.quantity - difference;
+        orderRevenue += (storage[order.name].quantity * storage[order.name].price)
+        order.totalPrice = (storage[order.name].quantity * storage[order.name].price)
+        storage[order.name].quantity = 0
+        return order
+       } else {
+        orderRevenue += (order.quantity * storage[order.name].price)
+        order.totalPrice = (storage[order.name].quantity * storage[order.name].price)
+       storage[order.name].quantity - order.quantity
+        return order
+       }
+     }
+    })
+  },
+}
+
+module.exports = {
+  increase,
+  double,
+  totalArray,
+  stringVariator,
+  mergeSort,
+  calculator,
+  dogHome
+};
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,4 +1,11 @@
-const { increase, totalArray } = require("../src/index.js");
+const {
+  increase,
+  totalArray,
+  stringVariator,
+  mergeSort,
+  calculator,
+  dogHome
+} = require("../src/index.js");
 
 test("increase", function() {
   const arg = 10;
@@ -20,4 +27,45 @@ test("increase", function() {
 //   expect(result).toBe(expected);
 // });
 
-test("add strings", function() {});
+test("add strings", function() {
+  const arg = "what";
+  const inner = stringVariator();
+  const result = inner(arg);
+  const expected = "what.";
+
+  expect(result).toBe(expected);
+});
+
+test("mergeNumber", function() {
+  const arg = 6;
+  const inner = mergeSort();
+  const result = inner(arg);
+  const expected = 12;
+
+  expect(result).toBe(expected);
+});
+
+test("calculate", function() {
+  const argNumber1 = 6;
+  const argOperator = "+";
+  const argNumber2 = 12;
+  const inner = calculator();
+  const result = inner(argNumber1, argOperator, argNumber2);
+  const expected = 18;
+
+  expect(result).toBe(expected);
+});
+
+test("dogHome", function() {
+  const arg = {
+    name: "rex",
+    breed: "Poodle",
+    colour: "Blue",
+    location: "Ipswich"
+  };
+  const inner = dogHome();
+  const result = inner.houseDog(arg);
+  const expected = { Ipswich: [arg] };
+
+  expect(result).toEqual(expected);
+});
